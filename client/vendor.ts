819dafa7d0b31a286aa063653acf1fa84dafc479
Thinh Tran <ns.hermione@gmail.com>
2017-02-21 11:32:35 +0700

    init proj

---
+++ client/vendor.ts
@@ -0,0 +1,19 @@
+// Angular
+// import '@angular/common';
+// import '@angular/compiler';
+// import '@angular/core';
+// import '@angular/forms';
+// import '@angular/http';
+// import '@angular/platform-browser';
+// import '@angular/platform-browser-dynamic';
+// import '@angular/router';
+// RxJS
+import 'rxjs';
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
+import '../bower_components/SpinKit/scss/spinners/3-wave.scss';
+import '../bower_components/font-awesome/less/font-awesome.less';
+import '../node_modules/bootstrap/less/bootstrap.less';
+import './assets/scss/app.scss';
+
+import 'bootstrap';
